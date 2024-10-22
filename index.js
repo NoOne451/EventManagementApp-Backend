cda7e2d2446d3088f8daabec4681c1349f55af06
NoOne451 <ranjithdurai111@gmail.com>
2024-10-22 09:28:52 +0530

    prisma changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { eventsRouter } from './routes/events.js';
-import prisma, { connectPrisma } from './prisma/client.js';
+import { connectPrisma } from './prisma/client.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
